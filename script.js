e7e0e9bb29c506749cbf64fbf475cab6c12c5c32
Maryam Ibikunle <ibikunlemaryamibidoyin@gmail.com>
2025-12-07 10:30:15 +0100

    Finally got the wheel to spin

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let billAmount;
-const bill = document.getElementById("billAmount")
+const bill = document.getElementById("billAmount");
 bill.addEventListener('input', ()=>{
-    console.log(bill.value);
     billAmount = Math.abs(bill.value);
 })
 
@@ -10,21 +9,30 @@ let tip;
 const percentButtons = document.querySelectorAll(".percent");
 
 percentButtons.forEach(button => button.addEventListener('click', ()=>{
-    console.log(billAmount)
+    console.log(billAmount);
     if (button.textContent == "10%")tip = 0.1*billAmount;
-    else if (button.textContent == "15%") tip = 0.15 * billAmount
-    else if (button.textContent == "20%") tip = 0.2*billAmount
-    console.log(tip)
+    else if (button.textContent == "15%") tip = 0.15 * billAmount;
+    else if (button.textContent == "20%") tip = 0.2*billAmount;
+    console.log("tip " + tip);
 }))
 
 let friendsNumber;
-const friends = document.getElementById("num-of-friends")
-bill.addEventListener('input', ()=>{
-    console.log(friends.value);
-    friendsNumber = friends.value;
+const friends = document.getElementById("num-of-friends");
+friends.addEventListener('input', ()=>{
+    friendsNumber = Math.abs(friends.value);
+    console.log("friends number: " + friendsNumber);
 })
 const split = document.getElementById("split")
 split.addEventListener('click', ()=>{
     console.log(friendsNumber);
     console.log(tip/friendsNumber);
+})
+
+let rotation = 150
+console.log(window.scrollY)
+const spinWheel = document.getElementById("spin");
+spinWheel.addEventListener('click', ()=>{
+    const random = Math.floor(2000+Math.random() * 3000);
+    rotation+=random
+    spinWheel.style.transform = `rotate(${rotation}deg)`
 })
